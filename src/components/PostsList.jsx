9287a4b32ea381894465d9601bf1725233291170
iamohsung <iamohsung@gmail.com>
2024-02-14 22:33:08 +0900

    Process form submissions.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,43 +5,14 @@ import classes from './PostsList.module.css';
 import Modal from './Modal';
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState();
-  const [enteredAuthor, setEnteredAuthor] = useState();
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  // let modalContent;
-
-  // if (modalIsVisible) {
-  //   modalContent = (
-  //     <Modal onClose={hideModalHandler}>
-  //       <NewPost
-  //         onBodyChange={bodyChangeHandler}
-  //         onAuthorChange={authorChangeHandler}
-  //       />
-  //     </Modal>
-  //   );
-  // }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author='OhSung' body='Check ouit the full course!' />
       </ul>
     </>
