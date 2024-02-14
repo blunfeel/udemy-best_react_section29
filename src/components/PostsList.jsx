6a2996116bc01d7829594f5b7458473e4ecd7515
iamohsung <iamohsung@gmail.com>
2024-02-14 22:41:19 +0900

    Update State based on previous State.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,11 +5,17 @@ import classes from './PostsList.module.css';
 import Modal from './Modal';
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
       <ul className={classes.posts}>
