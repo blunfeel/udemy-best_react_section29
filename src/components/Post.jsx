28de1e2380ee636925a8089792429392d0f5ae53
iamohsung <iamohsung@gmail.com>
2024-02-14 14:46:36 +0900

    Print Dynamic value

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ['Iamohsung', 'Ohsung'];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Iamohsung</p>
+      <p>{chosenName}</p>
       <p>React.js is awsome!</p>
     </div>
   );
