4a75c789ff5cfb44aab2ebcc14e3713387976303
iamohsung <iamohsung@gmail.com>
2024-02-15 21:32:05 +0900

    Send a POST Http requests.

--- frontend/src/components/PostsList.jsx
+++ frontend/src/components/PostsList.jsx
@@ -7,7 +7,14 @@ import Modal from './Modal';
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
-  function addPostHandler(postData) {
+  async function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
